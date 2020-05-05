21b87088761d9d10279dd2840341d44612940256
BrianDX <boris.zaitsev@devexpress.com>
2020-05-05 21:32:05 +0300

    update 20.1 new approach

---
+++ CS/SimpleAspNetCoreReportingDesigner/Reports/TestReport.cs
@@ -0,0 +1,13 @@
+using System;
+using DevExpress.XtraReports.UI;
+
+namespace SimpleAspNetCoreReportingDesigner.Reports
+{
+    public partial class TestReport
+    {
+        public TestReport()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CS/SimpleAspNetCoreReportingDesigner/Startup.cs
@@ -0,0 +1,61 @@
+﻿using DevExpress.AspNetCore;
+using DevExpress.AspNetCore.Reporting;
+using DevExpress.XtraReports.Web.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SimpleAspNetCoreReportingDesigner
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDevExpressControls();
+            services.AddScoped<ReportStorageWebExtension, Services.CustomReportStorageWebExtension>();
+            services
+                .AddMvc()
+                .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
+            services.ConfigureReportingServices(configurator =>
+            {
+                configurator.ConfigureReportDesigner(designerConfigurator =>
+                {
+                    designerConfigurator.RegisterDataSourceWizardConfigFileConnectionStringsProvider();
+                });
+                configurator.ConfigureWebDocumentViewer(viewerConfigurator =>
+                {
+                    viewerConfigurator.UseCachedReportSourceBuilder();
+                });
+            });
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            DevExpress.XtraReports.Configuration.Settings.Default.UserDesignerOptions.DataBindingMode = DevExpress.XtraReports.UI.DataBindingMode.Expressions;
+            app.UseDevExpressControls();
+            System.Net.ServicePointManager.SecurityProtocol |= System.Net.SecurityProtocolType.Tls12;
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
